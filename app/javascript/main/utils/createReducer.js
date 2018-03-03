3270240d06c2d9c98c3856b0749c66e45fd4ad27
Derek Kniffin <derek.kniffin@gmail.com>
2018-03-02 22:46:49 -0500

    Added remaining reducer handlers

--- app/javascript/main/utils/createReducer.js
+++ app/javascript/main/utils/createReducer.js
@@ -7,18 +7,25 @@ function createItemReducer(resource, operations) {
   const INITIAL_STATE = new Immutable.Map({
     isActive: false,
     isFetched: false,
-    data: new Immutable.Map()
+    data: new Immutable.Map(),
+    error: null
   });
 
   return function reducer(state = INITIAL_STATE, action) {
     switch (action.type) {
     case `${actionPrefix}_RETRIEVE_START`:
+    case `${actionPrefix}_CREATE_START`:
+    case `${actionPrefix}_UPDATE_START`:
+    case `${actionPrefix}_DELETE_START`:
       return state.withMutations((map) => {
         map.delete('error');
         map.set('isActive', true);
       });
 
     case `${actionPrefix}_RETRIEVE_SUCCESS`:
+    case `${actionPrefix}_CREATE_SUCCESS`:
+    case `${actionPrefix}_UPDATE_SUCCESS`:
+    case `${actionPrefix}_DELETE_SUCCESS`:
       return state.withMutations((map) => {
         map.set('isActive', false);
         map.set('isFetched', true);
@@ -26,6 +33,9 @@ function createItemReducer(resource, operations) {
       });
 
     case `${actionPrefix}_RETRIEVE_FAILURE`:
+    case `${actionPrefix}_CREATE_FAILURE`:
+    case `${actionPrefix}_UPDATE_FAILURE`:
+    case `${actionPrefix}_DELETE_FAILURE`:
       return state.withMutations((map) => {
         map.set('error', action.payload.error);
         map.set('isActive', false);
