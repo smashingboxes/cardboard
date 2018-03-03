8f932fd4462d654c2088ab0a720e46f3b2ed0840
Derek Kniffin <derek.kniffin@gmail.com>
2018-03-02 22:20:07 -0500

    Added reducer item function

--- app/javascript/main/utils/createReducer.js
+++ app/javascript/main/utils/createReducer.js
@@ -1,8 +1,39 @@
 import Immutable from 'immutable';
 
 function createItemReducer(resource, operations) {
-  return function reducer(state = {}, action) {
-    return state;
+  // eg PROJECTS
+  const actionPrefix = resource.toUpperCase();
+
+  const INITIAL_STATE = new Immutable.Map({
+    isActive: false,
+    isFetched: false,
+    data: new Immutable.Map()
+  });
+
+  return function reducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+    case `${actionPrefix}_RETRIEVE_START`:
+      return state.withMutations((map) => {
+        map.delete('error');
+        map.set('isActive', true);
+      });
+
+    case `${actionPrefix}_RETRIEVE_SUCCESS`:
+      return state.withMutations((map) => {
+        map.set('isActive', false);
+        map.set('isFetched', true);
+        map.set('data', Immutable.fromJS(action.payload.response));
+      });
+
+    case `${actionPrefix}_RETRIEVE_FAILURE`:
+      return state.withMutations((map) => {
+        map.set('error', action.payload.error);
+        map.set('isActive', false);
+      });
+
+    default:
+      return state;
+    }
   };
 }
 
