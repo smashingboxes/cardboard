7cffb29ab68419695d2b701943d9cd2dcca6c3fb
Derek Kniffin <derek.kniffin@gmail.com>
2018-03-09 17:01:28 -0500

    Use redux-form components

--- app/javascript/main/components/Stories/New.jsx
+++ app/javascript/main/components/Stories/New.jsx
@@ -4,6 +4,7 @@ import {
   Field
 } from 'redux-form';
 
+import { Text } from '../FormFields';
 
 const propTypes = {
   handleSubmit: PropTypes.func.isRequired
@@ -16,12 +17,18 @@ function NewStory(props) {
   return (
     <form onSubmit={handleSubmit}>
       <Field component="input" type="hidden" name="projectId" />
-      <span>Slug:</span>
-      <Field component="input" type="text" name="slug" />
-      <br />
-      <span>Summary:</span>
-      <Field component="input" type="text" name="summary" />
-      <br />
+
+      <Field
+        component={Text}
+        label="Slug"
+        name="slug"
+      />
+
+      <Field
+        component={Text}
+        label="Summary"
+        name="summary"
+      />
 
       <input type="submit" />
     </form>
